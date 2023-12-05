6f6c408f4e5fa4f32b54730db4db95fe9106ccdb
Marco Aurélio Santos de Meneses <54331005+M4rc0R@users.noreply.github.com>
2023-12-05 12:36:48 -0300

    Update config.js
    Delete key

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL ='Olá, <strong>{0}</strong> seja bem vindo ao Node Store';
 
 module.exports = {
     connectionString: 'mongodb://balta:e296cd9f@localhost:27017/admin',
-    sendgridKey: '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',
+    sendgridKey: 'keyboard',
     containerConnectionString: 'TBD'
 }
